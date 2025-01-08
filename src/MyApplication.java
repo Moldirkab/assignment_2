d6979dcf0ae110491aac0805b19c53a6e0c13057
Moldirkab <moldirkabdykarim@gmail.com>
2025-01-08 18:42:54 +0600

    Made a list of type Payable and modified iterable objects to handle Payable instances

--- src/MyApplication.java
+++ src/MyApplication.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 public class MyApplication {
     public static void main(String[] args) {
-        List<Person> people = new ArrayList<>();
+        List<Payable> people = new ArrayList<>();
        people.add(new Employee("John", "Lennon", "lawyer", 27045.98));
        people.add(new Employee("George", "Harrison","teacher", 50000.00));
        people.add(new Student("Ringo", "Star", 2.65));
@@ -16,7 +16,7 @@ public class MyApplication {
         printData(people);
     }
 
-    public static void printData(Iterable<Person> payables) {
+    public static void printData(Iterable<Payable> payables) {
         payables.forEach(payable -> {
             System.out.println(payable.toString() +  " earns " + payable.getPaymentAmount() + " tenge");
         });
